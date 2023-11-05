03183e8d11c9e154f689502c74316550e4bff5b0
simplystudios <new.anshwadhwa@gmail.com>
2023-11-05 22:51:12 +0530

    navigation fixed

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -27,8 +27,8 @@ function openmenu(){
                 <div id="navpc" class="nav_links">
                     <ul style="background-color: var(--bcsecond)">
                         <li><a href="/home">Home</a></li>
-                        <li><a href="/">Trending</a></li>
-                        <li><a href="/">Top</a></li>
+                        <li><a href="/home">Trending</a></li>
+                        <li><a href="/home">Top</a></li>
                         <li><a href="/about">About</a></li>
                     </ul>
              </div>
@@ -37,8 +37,8 @@ function openmenu(){
             <div id="nav" class="nav_links_moble">
                 <ul>
                     <li><a href="/home">Home</a></li>
-                    <li><a href="/">Trending</a></li>
-                    <li><a href="/">Top</a></li>
+                    <li><a href="/home">Trending</a></li>
+                    <li><a href="/home">Top</a></li>
                     <li><a href="/about">About</a></li>
                 </ul>
             </div>
